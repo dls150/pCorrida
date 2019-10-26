792eb5ff58f15ab7cbd6f8066f5692d718b626f5
dls150 <150105@upf.br>
2019-10-26 20:46:18 -0300

    Initial Commit

---
+++ sobre.php
@@ -0,0 +1,13 @@
+<?php
+require_once "./cabecalho.php";?>
+
+<div class="container">
+	<div class="jumbotron">
+		<h1></h1>
+		<h4>Sobre</h4>
+		<br><br> Desenvolvido por DLS - 150105
+		<br><br> Projeto 2019
+	</div>
+</div>
+
+<?php require_once "./rodape.php"; ?>
